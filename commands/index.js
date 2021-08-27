fd0738e3f28fb79e879d8d2b4eb567086c1573cd
Agus <arenasagustin7@gmail.com>
2021-08-26 23:24:32 -0300

    Inventos

--- commands/index.js
+++ commands/index.js
@@ -4,39 +4,20 @@ const axios = require('axios'); //npm i axios
 
 const print = (arg) => process.stdout.write(arg);
 const printPromp = () => print('\nI´m Goot > ');
-const helpList = [ 'cowsay', 'cowthink', 'cat', 'curl', 'date', 'echo', 'head', 'help', 'ls', 'mrTurnerSay', 'pwd', 'randomsay', 'randomthink', 'tail' ];
 
-module.exports = {
-	help: () => {
-		print(`\n\t- ${helpList.join('\n\t')}\n`);
-		printPromp();
-	},
+let helpList = [];
 
-    date: () => {
-    	print(`\t${Date()}`);
-    	printPromp();
-    },
+const timer = (seg) => {
+	fs.readFile(__dirname + `/text/counter/${seg}.txt`, 'utf8', (err, data) => {
+	 	if(err) throw err;
 
-    pwd: () => {
-    	print(`\t${process.cwd()}`);
-    	printPromp();
-	},
+		print(`${data}`);
+		if(seg === 1) printPromp();
+	})
+}
 
-    echo: (arg) => {
-    	print(`\t${arg.join(' ')}`);
-    	printPromp();
-    },
-
-    ls: () => {
-    	fs.readdir('.', (err, files) => {
-			if (err) throw err;
-
-			print(`\n\t${files.join('\n\t')}\n`);
-			printPromp();
-		});
-    },
-
-    cat: (arg) => {
+const cmd = {
+	cat: (arg) => {
     	fs.readFile(__dirname + `/text/${arg}.txt`, 'utf8', (err, data) => {
 		 	if(err) throw err;
 
@@ -45,48 +26,12 @@ module.exports = {
 		})
     },
 
-    head: (arg) => {
-    	fs.readFile(__dirname + `/text/${arg}.txt`, 'utf8', (err, data) => {
-		 	if(err) throw err;
-		 	let aux = data.split('\n').slice(0, 6);
-
-	    	print(`\n\n${aux.join('\n')}\n`);
-	    	printPromp();
-    	})
+    count5s: () => {
+    	for(i = 5; i > 0; i--){
+    		setTimeout(timer(i), 1000);
+    	}
     },
 
-    tail: (arg) => {
-    	fs.readFile(__dirname + `/text/${arg}.txt`, 'utf8', (err, data) => {
-		 	if(err) throw err;
-		 	let aux = data.split('\n').slice(-6);
-
-	    	print(`\n\n${aux.join('\n')}\n`);
-	    	printPromp();
-    	})
-    },
-
-    curl: function(arg) {
-        axios(arg[0])
-        .then(function(res) {
-            print(res.data.toString());
-        })
-        .catch(err => {
-            print(err);
-        })
-
-        printPromp();
-    },
-
-    mrTurnerSay: (arg) => {
-    	fs.readFile(__dirname + '/text/mrturner.txt', 'utf8', (err, data) => {
-		 	if(err) throw err;
-
-	    	print(`\n\n\t\t\t\t\t\t\t${arg}\n`);
-			print(`${data}\n`);
-			printPromp();
-    	})
-    },
-	
     cowsay: (arg) => {  
     	print(cowsay.say({
     		text : `${arg}`,
@@ -105,6 +50,77 @@ module.exports = {
     	}));
 
     	printPromp();
+    },
+
+    curl: function(arg) {
+        axios(arg[0])
+        .then(function(res) {
+            print(res.data.toString());
+        })
+        .catch(err => {
+            print(err);
+        })
+
+        printPromp();
+    },
+
+    date: () => {
+    	print(`\t${Date()}`);
+    	printPromp();
+    },
+
+    echo: (arg) => {
+    	print(`\t${arg.join(' ')}`);
+    	printPromp();
+    },
+    
+    head: (arg) => {
+    	fs.readFile(__dirname + `/text/${arg}.txt`, 'utf8', (err, data) => {
+		 	if(err) throw err;
+		 	let aux = data.split('\n').slice(0, 6);
+
+	    	print(`\n\n${aux.join('\n')}\n`);
+	    	printPromp();
+    	})
+    },
+
+	help: () => {
+		print(`\n\t- ${helpList.join('\n\t- ')}\n`);
+		printPromp();
+	},
+
+    ls: () => {
+    	fs.readdir('.', (err, files) => {
+			if (err) throw err;
+
+			print(`\n\t${files.join('\n\t')}\n`);
+			printPromp();
+		});
+    },
+    
+    mrTurnerSay: (arg) => {
+    	fs.readFile(__dirname + '/text/mrturner.txt', 'utf8', (err, data) => {
+		 	if(err) throw err;
+
+	    	print(`\n\n\t\t\t\t\t\t\t${arg}\n`);
+			print(`${data}\n`);
+			printPromp();
+    	})
+    },
+
+    pwd: () => {
+    	print(`\t${process.cwd()}`);
+    	printPromp();
+	},
+
+    tail: (arg) => {
+    	fs.readFile(__dirname + `/text/${arg}.txt`, 'utf8', (err, data) => {
+		 	if(err) throw err;
+		 	let aux = data.split('\n').slice(-6);
+
+	    	print(`\n\n${aux.join('\n')}\n`);
+	    	printPromp();
+    	})
     },
 
     randomsay: (arg) => {  
@@ -127,3 +143,7 @@ module.exports = {
     	printPromp();
     },
 }
+
+helpList = Object.getOwnPropertyNames(cmd)
+
+module.exports = cmd;
