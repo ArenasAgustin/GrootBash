45c67633b6ae47f0f27754e959400c1dda83e207
Agus <arenasagustin7@gmail.com>
2021-09-03 17:03:06 -0300

    Refactoring, new comand and new readme

--- commands/index.js
+++ commands/index.js
@@ -1,145 +1,140 @@
 const fs = require('fs'); //npm i fs
 const cowsay = require('cowsay'); //npm i cowsay
-const axios = require('axios'); //npm i axios
-var Promise = require('bluebird');
+const request = require('request'); //npm i request
+
+const {
+  print,
+  printConPromp,
+  readFile,
+  reader,
+  timer,
+  promisifiedtimer,
+  counter
+} = require ('./utils.js')
 
 let helpList = [];
 
-const print = arg => process.stdout.write(arg);
-const printConPromp = arg => {
-	print(arg);
-	print('\nI´m Groot > ');
-};
-
-const readFile = (filename, callback) => {
-	fs.readFile(filename, (err, buffer) => {
-		if(err) callback(err);
-		else callback(null, buffer.toString());
-	});
-};
-
-const reader = filename => {
-	return new Promise((resolve, reject) => {
-		readFile(filename, (err, str) => {
-			if(err) reject(err);
-			else resolve(str);
-		});
-	});
-};
-
 const cmd = {
 	cat: arg => {
-		reader(`${__dirname}/text/${arg}.txt`)
-			.then(data => printConPromp(`${data}`))
-			.catch(err => printConPromp(`${err}`))
-    },
+	reader(`${__dirname}/text/${arg}.txt`)
+		.then(data => printConPromp(`${data}`))
+		.catch(err => printConPromp(`${err}`))
+  },
 
-    cowsay: arg => {  
-    	printConPromp(cowsay.say({
-    		text : `${arg}`,
-    		e : 'oO',
-    		T : 'U '
-    	}));
-    },
+  count: seg => {
+  	counter(seg);
+  },
 
-    cowthink: arg => {  
-    	printConPromp(cowsay.think({
-    		text : `${arg}`,
-    		e : '==',
-    		T : 'U '
-    	}));
-    },
+  cowsay: arg => {  
+  	printConPromp(`\n${cowsay.say({
+  	  		text : `${arg}`,
+  	  		e : 'oO',
+  	  		T : 'U '
+  	  	})}\n`);
+  },
 
-    curl: arg => {
-        axios(arg[0])
-	        .then(res => printConPromp(res.data.toString()))
-	        .catch(err => printConPromp(err));
-    },
+  cowthink: arg => {  
+  	printConPromp(`\n${cowsay.think({
+  		text : `${arg}`,
+  		e : '==',
+  		T : 'U '
+  	})}\n`);
+  },
 
-    date: () => {
-    	printConPromp(`\t${Date()}`);
-    },
+  curl: arg => {
+      request(arg[0], function(err, res, body){
+      	if(err) printConPromp(err);
 
-    echo: arg => {
-    	printConPromp(`\t${arg.join(' ')}`);
-    },
+      	else printConPromp(body);
+      })
+  },
+
+  date: () => {
+  	printConPromp(`\t${Date()}`);
+  },
+
+  echo: arg => {
+  	printConPromp(`\t${arg.join(' ')}`);
+  },
     
-    head: arg => {
-    	reader(`${__dirname}/text/${arg}.txt`)
-			.then(data => {
-				let aux = data.split('\n').slice(0, 6);
+  head: arg => {
+  	reader(`${__dirname}/text/${arg[0]}.txt`)
+		.then(data => {
+			const lines = arg[1] ? parseInt(arg[1]) : 6;
+			let aux = data.split('\n').slice(0, lines);
 
-				printConPromp(`\n\n${aux.join('\n')}\n`);
-			})
-			.catch(err => printConPromp(`${err}`));
-    },
+			printConPromp(`\n\n${aux.join('\n')}\n`);
+		})
+		.catch(err => printConPromp(`${err}`));
+  },
 
 	help: () => {
 		printConPromp(`\n\t- ${helpList.join('\n\t- ')}\n`);
 	},
 
-    ls: () => {
-    	fs.readdir('.', (err, files) => {
-			if (err) printConPromp(err);
+  ls: () => {
+  	fs.readdir('.', (err, files) => {
+		if (err) printConPromp(err);
 
-			printConPromp(`\n\t${files.join('\n\t')}\n`);
-		});
-    },
-    
-    mts: arg => {
-    	reader(`${__dirname}/text/mrturner.txt`)
-			.then(data => {
-				let aux = data.split('\n').slice(-6);
+		else printConPromp(`\n\t${files.join('\n\t')}\n`);
+	});
+  },
+  
+  mts: arg => {
+  	reader(`${__dirname}/text/mrturner.txt`)
+		.then(data => {
+			let aux = data.split('\n').slice(-6);
 
-				print(`\n\n\t\t\t\t\t\t\t${arg}\n`);
-				printConPromp(`${data}\n`);
-			})
-			.catch(err => printConPromp(`${err}`));
-    },
+			print(`\n\n\t\t\t\t\t\t\t${arg}\n`);
+			printConPromp(`${data}\n`);
+		})
+		.catch(err => printConPromp(`${err}`));
+  },
 
-    pwd: () => {
-    	printConPromp(`\t${process.cwd()}`);
+  pwd: () => {
+  	printConPromp(`\t${process.cwd()}`);
 	},
 
-    randomsay: arg => {  
-    	printConPromp(cowsay.say({
-    		text : `${arg}`,
-    		e: '^^',
-    		r: true,
-    	}));
-    },
+  randomsay: arg => {  
+  	printConPromp(`\n${cowsay.say({
+  		text : `${arg}`,
+  		e: '^^',
+  		r: true,
+  	})}\n`);
+  },
 
-    randomthink: arg => {  
-    	printConPromp(cowsay.think({
-    		text : `${arg}`,
-    		e: '^^',
-    		r: true,
-    	}));
-    },
+  randomthink: arg => {  
+  	printConPromp(`\n${cowsay.think({
+  		text : `${arg}`,
+  		e: '^^',
+  		r: true,
+  	})}\n`);
+  },
 
-    sort: () => printConPromp(`\n\nComing soon\n`),
+  sort: () => printConPromp(`\n\nComing soon\n`),
 
-    tail: arg => {
-    	reader(`${__dirname}/text/${arg}.txt`)
-			.then(data => {
-				let aux = data.split('\n').slice(-6);
+  tail: arg => {
+  	reader(`${__dirname}/text/${arg[0]}.txt`)
+		.then(data => {
+			const lines = arg[1] ? parseInt(arg[1]) : 6;
+			let aux = data.split('\n').slice(-lines);
 
-				printConPromp(`\n\n${aux.join('\n')}\n`);
-			})
-			.catch(err => printConPromp(`${err}`));
-    },
+			printConPromp(`\n\n${aux.join('\n')}\n`);
+		})
+		.catch(err => printConPromp(`${err}`));
+  },
 
-    uniq: () => printConPromp(`\n\nComing soon\n`),
+  uniq: () => printConPromp(`\n\nComing soon\n`),
 
-    wc: arg => {
-    	reader(`${__dirname}/text/${arg}.txt`)
-			.then(data => {
-				let aux = data.split('\n');
+  wc: arg => {
+  	reader(`${__dirname}/text/${arg}.txt`)
+		.then(data => {
+			let aux = data.split('\n');
 
-				printConPromp(`\n\nlines: ${aux.length}\n`);
-			})
-			.catch(err => printConPromp(`${err}`));
-    },
+			printConPromp(`\n\nlines: ${aux.length}\n`);
+		})
+		.catch(err => printConPromp(`${err}`));
+  },
 }
 
 helpList = Object.getOwnPropertyNames(cmd)
---
+++ commands/utils.js
@@ -0,0 +1,63 @@
+const fs = require('fs'); //npm i fs
+const Promise = require('bluebird');
+
+const print = arg => process.stdout.write(arg);
+
+const printConPromp = arg => {
+  print(arg);
+  print('\nI´m Groot > ');
+};
+
+const readFile = (filename, callback) => {
+  fs.readFile(filename, (err, buffer) => {
+    if(err) callback(err);
+    else callback(null, buffer.toString());
+  });
+};
+
+const reader = filename => {
+  return new Promise((resolve, reject) => {
+    readFile(filename, (err, str) => {
+      if(err) reject(err);
+      else resolve(str);
+    });
+  });
+};
+
+const timer = (seg, cb) => {
+  setTimeout(() => {
+    cb(seg)
+  }, 1000);
+};
+
+const promisifiedtimer = (seg) => {
+	return new Promise((resolve, reject) => {
+		timer(seg, (str, err) => {
+			if (err) reject(err);
+			else resolve(str);
+		});
+	});
+};
+
+const counter = async seg => {
+  seg = parseInt(seg);
+  if(!typeof seg === 'number' || seg < 1) printConPromp('Solo numeros positivos')
+  
+  else {
+    for(let i = seg; i >= 0; i--){
+      var j = await promisifiedtimer(i);
+      print(`\n${j}`)
+    }
+  }
+  printConPromp('\ndone\n')
+};
+
+module.exports = {
+  print,
+  printConPromp,
+  readFile,
+  reader,
+  timer,
+  promisifiedtimer,
+  counter
+}
